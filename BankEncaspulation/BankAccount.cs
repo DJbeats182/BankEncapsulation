d7b34b7f177172de7466e61a36b7ca337dfd4594
DJ Bates <blooviper@gmail.com>
2022-08-19 10:16:06 -0600

    First Commit

---
+++ BankEncaspulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncaspulation
+{
+    public class BankAccount
+    {
+        private static double balance = 15_000;
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public static double DepositBalance(double amount)
+        {
+           return balance += amount;
+        }
+
+        public static bool PinVerification(bool validCheck, int pin)
+        {
+            if (validCheck && pin == 1234)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ BankEncaspulation/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+using BankEncaspulation;
+
+BankAccount djsAccount = new BankAccount();
+
+Console.WriteLine("Welcome to your Bank Account!\n");
+Console.WriteLine("Please enter your PIN to get started:");
+bool isAuthenticated;
+
+do
+{
+    bool isNumber = int.TryParse(Console.ReadLine(), out int pin);
+    isAuthenticated = BankAccount.PinVerification(isNumber, pin);
+
+    if (isAuthenticated)
+    {
+        Console.WriteLine("Welcome to your account!\n");
+        Console.WriteLine($"Your current Balance is: {djsAccount.GetBalance()}\n");
+    }
+    else
+    {
+        Console.WriteLine("Sorry that PIN is incorrect\n");
+        Console.WriteLine("Please Try Again!");
+    }
+} while (!isAuthenticated);
+
+
+Console.WriteLine("Enter in an amount to deposit:");
+double depositAmount = double.Parse(Console.ReadLine());
+BankAccount.DepositBalance(depositAmount);
+Console.WriteLine($"Your new balance is: {djsAccount.GetBalance()}");
+
+
+//string pass = "";
+//Console.WriteLine("Please enter your PIN to get started:");
+//bool isNumber = int.TryParse(Console.ReadLine(),out int pin);
+//ConsoleKeyInfo key;
+
+//do
+//{
+//    key = Console.ReadKey(true);
+
+//    // Backspace Should Not Work
+//    if (key.Key != ConsoleKey.Backspace)
+//    {
+//        pass += key.KeyChar;
+//        Console.Write("*");
+//    }
+//    else
+//    {
+//        Console.Write("\b");
+//    }
+//}
+//// Stops Receving Keys Once Enter is Pressed
+//while (key.Key != ConsoleKey.Enter);
+
